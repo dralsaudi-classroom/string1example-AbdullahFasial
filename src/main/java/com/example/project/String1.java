1a06734d47a8713628ce63d9685cf2961021b344
AbdullahFasial <xqabdullhqx@gmail.com>
2024-10-18 18:32:56 +0300

    Update String1.java

--- src/main/java/com/example/project/String1.java
+++ src/main/java/com/example/project/String1.java
@@ -5,41 +5,50 @@ public class String1 {
     private int size;
 
     public String1() {
-        size = -1;
+        size = 0;
         str = new char[80];
     }
 
     public void append(char c) {
-        str[++size] = c;
+        str[size++] = c;
     }
 
     public char remove() {
-        return str[size--];
+        if (size == 0) {
+            throw new IllegalStateException("String is empty");
+        }
+        return str[--size];
     }
 
     public char getChar(int i) {
+        if (i < 0 || i >= size) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
         return str[i];
     }
 
     public int length() {
-        return size + 1;
+        return size;
     }
 
     public void makeEmpty() {
-        size = -1;
+        size = 0;
     }
 
     public void concat(String1 s) {
-        for (int i = 0; i < s.length(); i++)
+        for (int i = 0; i < s.length(); i++) {
             append(s.getChar(i));
+        }
     }
 
     public boolean equal(String1 s) {
-        if (this.length() != s.length()) 
+        if (this.length() != s.length()) {
             return false;
+        }
         for (int i = 0; i < this.length(); i++) {
-            if (this.getChar(i) != s.getChar(i))
+            if (this.getChar(i) != s.getChar(i)) {
                 return false;
+            }
         }
         return true;
     }
@@ -47,8 +56,8 @@ public class String1 {
     public void reverse() {
         for (int i = 0; i < this.length() / 2; i++) {
             char temp = str[i];
-            str[i] = str[size - i];
-            str[size - i] = temp;
+            str[i] = str[size - i - 1];
+            str[size - i - 1] = temp;
         }
     }
 }
